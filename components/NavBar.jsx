cc4d58bef529e5b8a36cd94b12145674114f3bf6
Mercy Okwolim <mercyokwoli4@gmail.com>
2024-04-26 16:12:17 +0100

    implemented bvn-search

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
 import { Menu, X } from "react-feather";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const router =useRouter()
+  
   return (
     <nav className="py-4 md:py-0 px-4 bg-white w-full flex justify-between items-center shadow-lg">
       <button
@@ -13,22 +16,26 @@ function NavBar() {
         {menuOpen ? <X /> : <Menu />}
       </button>
       <ul
-        className={` absolute bg-white rounded-xl flex-col shadow-slate-500 shadow-xl w-4/5 left-1/2 top-16   transition-transform duration-200 transform -translate-x-1/2 origin-top ${
+        className={` z-50 absolute bg-white rounded-xl flex-col shadow-slate-500 shadow-xl w-4/5 left-1/2 top-16   transition-transform duration-200 transform -translate-x-1/2 origin-top ${
           menuOpen ? "translate-y-0" : "-translate-y-[150%]"
         } md:translate-y-0 flex md:bg-red md:top-0 md:left-0 md:translate-x-0 md:static md:flex-row md:px-10 md:shadow-none `}
       >
         <li>
           <Link
-            className="py-6 px-8 md:px-4 inline-block w-full hover:bg-slate-50 hover:text-black  "
-            href={"#"}
+            className={`py-6 px-8 md:px-4 inline-block w-full hover:bg-slate-50 hover:text-black ${
+              router.pathname == "/" ? "text-black font-bold" : ""
+            } `}
+            href={"/"}
           >
             Home
           </Link>
         </li>
         <li>
           <Link
-            className="py-6 px-8 md:px-4 inline-block w-full hover:bg-slate-50 hover:text-black text-slate-500 "
-            href={"#"}
+            className={`py-6 px-8 md:px-4 inline-block w-full hover:bg-slate-50 hover:text-black text-slate-500 ${
+              router.pathname == "/bvn-search" ? "text-black font-bold" : ""
+            } `}
+            href={"/bvn-search"}
           >
             BVN Search
           </Link>
@@ -43,7 +50,9 @@ function NavBar() {
         </li>
         <li>
           <Link
-            className="py-6 px-8 md:px-4 inline-block w-full hover:bg-slate-50 hover:text-black text-slate-500 "
+            className={`py-6 px-8 md:px-4 inline-block w-full hover:bg-slate-50 hover:text-black text-slate-500 ${
+              router.pathname == "/apply-loan" ? "text-black font-bold" : ""
+            } `}
             href={"/apply-loan"}
           >
             Apply Loan
@@ -51,14 +60,14 @@ function NavBar() {
         </li>
       </ul>
       <div className="flex gap-5">
-        <Link href="/sign-up">
-          <button className="p-1.5 md:p-2 md:px-2.5 bg-white border-black border-2 rounded-lg hover:bg-black hover:text-white transition-colors">
-            Sign Up
+        <Link href="/sign-in">
+          <button className="p-1.5 md:p-2 md:px-2.5 bg-white  rounded-lg hover:bg-green-500 hover:text-white transition-colors duration-100">
+            Sign In
           </button>
         </Link>
-        <Link href="/sign-in">
-          <button className="p-1.5 md:p-2 md:px-2.5 bg-white border-black border-2 rounded-lg hover:bg-black hover:text-white transition-colors">
-            Sign In
+        <Link href="/sign-up">
+          <button className="p-1.5 md:p-2 md:px-2.5 bg-white border-black border-2 rounded-lg hover:bg-black hover:text-white hover:border-none transition-all duration-100">
+            Sign Up
           </button>
         </Link>
       </div>
---
+++ pages/bvn-search.jsx
@@ -0,0 +1,65 @@
+import NavBar from "@/components/NavBar";
+import { useState, useEffect } from "react";
+import { Search } from "react-feather";
+import RingLoader from "react-spinners/RingLoader";
+
+function bvnSearch() {
+  const [showSearchLoading, setShowSearchLoading] = useState(false);
+  const [showSearchResults, setShowSearchResults] = useState(false);
+  useEffect(() => {
+    if (showSearchLoading) {
+      setTimeout(() => {
+        console.log(4090);
+        setShowSearchLoading(false);
+        setShowSearchResults(true);
+      }, 3000);
+    }
+  }, [showSearchLoading]);
+  return (
+    <>
+      <NavBar />
+      <main className="p-12">
+        <div className="text-center space-y-8">
+          <h1 className="text-2xl font-bold">BVN Eligibility Search</h1>
+          <p className="text-slate-500">
+            Please enter your BVN (Bank Verification Number) to see your credit
+            score.
+          </p>
+          <div className="relative w-4/5 md:max-w-[400px] mx-auto">
+            <input
+              type="text"
+              className="w-full rounded-lg border-gray-200 outline-green-500 p-4 pe-12 text-sm shadow-sm"
+              placeholder="BVN"
+            />
+            <button className="absolute bg-green-500 text-white rounded-e-lg inset-y-0 end-0 grid place-content-center px-4">
+              <Search
+                className=""
+                size={20}
+                onClick={() => {
+                  setShowSearchLoading(true);
+                  setShowSearchResults(false);
+                }}
+              />
+            </button>
+          </div>
+        </div>
+        {showSearchLoading && (
+          <div className="h-[200px] flex items-center justify-center flex-col">
+            <RingLoader color="green" />
+            <p className="text-green-500">Please wait...</p>
+          </div>
+        )}
+        {showSearchResults && (
+          <div className="h-[200px] flex items-center justify-center flex-col">
+            <p>
+              Dear [Name], Your credit score is [X] meaning your loan
+              eligibility is[X]
+            </p>
+          </div>
+        )}
+      </main>
+    </>
+  );
+}
+
+export default bvnSearch;
